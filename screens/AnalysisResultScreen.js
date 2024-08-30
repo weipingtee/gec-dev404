9523fb086639d7528557dfc11f9367a251e63791
Jia Qi <sihjiaqi@gmail.com>
2024-08-31 03:38:04 +0800

    fix css and edit color palette images

--- screens/AnalysisResultScreen.js
+++ screens/AnalysisResultScreen.js
@@ -6,6 +6,7 @@ import {
     TouchableOpacity
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import styles from '../analysis-result-style';
 
 function CustomButton({ title, onPress }) {
@@ -21,19 +22,19 @@ const AnalysisResultScreen = () => {
     const colorTone = 'cool summer'; // to be updated
 
     if (colorTone == 'cool summer') {
-        imageSource = require('/assets/cool-summer.jpg');
+        imageSource = require('../assets/cool-summer.png');
         subheadingText = 'Cool Summer';
         colorToneDescription = 'This palette is characterized by typical summer softness. The best colors are cool  shades, so blues and cool pink are heavily represented in the palette';
     } else if (colorTone == 'cool winter') {
-        imageSource = require('/assets/cool-winter.jpg');
+        imageSource = require('../assets/cool-winter.png');
         subheadingText = 'Cool Winter';
         colorToneDescription = 'This palette is defined by deep, vivid colors with a cool undertone. The best colors are rich jewel tones like emerald, sapphire, and royal blue, as well as icy shades of white and gray that complement the crisp, cool appearance of winter.';
     } else if (colorTone == 'warm autumn') {
-        imageSource = require('/assets/warm-autumn.jpg');
+        imageSource = require('../assets/warm-autumn.png');
         subheadingText = 'Warm Autumn';
         colorToneDescription = 'This palette is marked by earthy, rich tones that echo the warmth of autumn. The best colors include deep oranges, warm browns, and olive greens, reflecting the natural hues of falling leaves and harvest time.';
     } else if (colorTone == 'Spring') {
-        imageSource = require('/assets/warm-spring.jpg');
+        imageSource = require('../assets/warm-spring.png');
         subheadingText = 'Warm Spring';
         colorToneDescription = 'This palette features light, warm tones that are vibrant and fresh. The best colors are soft yellows, peach, coral, and light greens, reminiscent of blooming flowers and sunny spring days.';
     }
@@ -45,7 +46,7 @@ const AnalysisResultScreen = () => {
             <Image style={styles.colorPalette} source={imageSource} />
             <Image
                 style={styles.userImage}
-                source={require('/assets/cool-summer-user.jpg')}
+                source={require('../assets/cool-summer-user.jpg')}
             />
             <Text style={styles.subheadingText}>Cool Summer</Text>
             <View style={styles.line} />
