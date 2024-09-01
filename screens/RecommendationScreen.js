c4aeb11502f8caf450d51dd961d7b7d5aab333de
Rachel Tan <racheltanmz@gmail.com>
2024-09-01 10:44:22 +0800

    changed cards to touchable opacity

--- screens/RecommendationScreen.js
+++ screens/RecommendationScreen.js
@@ -12,78 +12,78 @@ import styles from '../home-style';
 const RecommendationScreen = () => (
     <ScrollView>
         <View style={styles.recommendSection}>
-            <View style={styles.recommendItem}>
+            <TouchableOpacity style={styles.recommendItem}>
                 <Image
                     source={require('../assets/red-top.jpg')}
                     style={styles.recommendImg}
                 />
                 <Text style={styles.recommendProduct}>Royal Blue Cotton Shirt</Text>
                 <Text style={styles.recommendPrice}>SG$35.87</Text>
-            </View>
-            <View style={styles.recommendItem}>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.recommendItem}>
                 <Image
                     source={require('../assets/blue-dress.png')}
                     style={styles.recommendImg}
                 />
                 <Text style={styles.recommendProduct}>Royal Blue Cotton Shirt</Text>
                 <Text style={styles.recommendPrice}>SG$28</Text>
-            </View>
-            <View style={styles.recommendItem}>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.recommendItem}>
                 <Image
                     source={require('../assets/olive-top.png')}
                     style={styles.recommendImg}
                 />
                 <Text style={styles.recommendProduct}>Royal Blue Cotton Shirt</Text>
                 <Text style={styles.recommendPrice}>SG$15</Text>
-            </View>
-            <View style={styles.recommendItem}>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.recommendItem}>
                 <Image
                     source={require('../assets/red-top.jpg')}
                     style={styles.recommendImg}
                 />
                 <Text style={styles.recommendProduct}>Royal Blue Cotton Shirt</Text>
                 <Text style={styles.recommendPrice}>SG$35.87</Text>
-            </View>
-            <View style={styles.recommendItem}>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.recommendItem}>
                 <Image
                     source={require('../assets/blue-dress.png')}
                     style={styles.recommendImg}
                 />
                 <Text style={styles.recommendProduct}>Royal Blue Cotton Shirt</Text>
                 <Text style={styles.recommendPrice}>SG$28</Text>
-            </View>
-            <View style={styles.recommendItem}>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.recommendItem}>
                 <Image
                     source={require('../assets/olive-top.png')}
                     style={styles.recommendImg}
                 />
                 <Text style={styles.recommendProduct}>Royal Blue Cotton Shirt</Text>
                 <Text style={styles.recommendPrice}>SG$15</Text>
-            </View>
-            <View style={styles.recommendItem}>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.recommendItem}>
                 <Image
                     source={require('../assets/red-top.jpg')}
                     style={styles.recommendImg}
                 />
                 <Text style={styles.recommendProduct}>Royal Blue Cotton Shirt</Text>
                 <Text style={styles.recommendPrice}>SG$35.87</Text>
-            </View>
-            <View style={styles.recommendItem}>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.recommendItem}>
                 <Image
                     source={require('../assets/blue-dress.png')}
                     style={styles.recommendImg}
                 />
                 <Text style={styles.recommendProduct}>Royal Blue Cotton Shirt</Text>
                 <Text style={styles.recommendPrice}>SG$28</Text>
-            </View>
-            <View style={styles.recommendItem}>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.recommendItem}>
                 <Image
                     source={require('../assets/olive-top.png')}
                     style={styles.recommendImg}
                 />
                 <Text style={styles.recommendProduct}>Royal Blue Cotton Shirt</Text>
                 <Text style={styles.recommendPrice}>SG$15</Text>
-            </View>
+            </TouchableOpacity>
         </View>
         <Text style={styles.pageEnd}>End of Page</Text>
     </ScrollView>
